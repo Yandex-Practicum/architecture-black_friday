f154ec3613a3256b2eb7c7c38de7df9bb45af868
krylovalexey <Kakalex208@gmail.com>
2025-12-10 02:51:06 +0300

    task 3, 4 corrections

--- sharding-repl-cache/scripts/router-init.sh
+++ sharding-repl-cache/scripts/router-init.sh
@@ -2,8 +2,8 @@
 
 docker exec -i mongos_router mongosh --port 27020 <<EOF
 
-sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "shard1/shard1-1:27018");
+sh.addShard( "shard2/shard2-1:27019");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
