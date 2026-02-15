c3339fb820ca7c08cbb12cb53c9545a4d4d2ac09
Lex <alexeyprutik@gmail.com>
2026-02-15 20:11:02 +0300

    Task 2 completed

---
+++ mongo-sharding/scripts/init_config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "===> Инициализация Config Servers"
+
+docker exec -i configSrv1  mongosh --quiet --eval '
+rs.initiate({
+  _id: "config_server",
+  configsvr: true,
+  members: [
+    { _id: 0, host: "configSrv1:27017" },
+    { _id: 1, host: "configSrv2:27017" },
+    { _id: 2, host: "configSrv3:27017" }
+  ]
+});
+'
+
+echo "===> Config Servers инициализированы"
